5cbf0bb27fada025722fb4bc68c013cefa56085e
Diego Cardoso <diegocar448@hotmail.com>
2026-03-09 17:35:20 -0300

    4.2 Chmod

--- backend/database/migrations/0001_01_02_000005_create_permissions_table.php
+++ backend/database/migrations/0001_01_02_000005_create_permissions_table.php
@@ -6,20 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique(); // ex: plans.create
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('permissions');
